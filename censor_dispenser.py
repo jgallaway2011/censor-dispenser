a160763f88314152b7c374b0fef276810c7ac3ae
jgallaway2011 <jgallaway2011@gmail.com>
2020-01-29 16:58:03 -0500

    Updates censor_text function

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -4,11 +4,25 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
+#Terms that should be censored
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+
 #Functions
 def censor_text(email, text_to_censor):
-    if text_to_censor in email:
-        email_censored = email.replace(text_to_censor, "*" * len(text_to_censor))
+    censored_text = ""
+    for i in range(len(text_to_censor)):
+        if text_to_censor[i] == " ":
+            censored_text += " "
+        else:
+            censored_text += "*"
+    email_censored = email.replace(text_to_censor, censored_text)
+    return email_censored
+
+def censor_text_with_list(email, list_of_text_to_censor):
+    for text in list_of_text_to_censor:
+        email_censored = email.replace(text, "*" * len(text))
     return email_censored
 
 #Function Calls
 print(censor_text(email_one, "learning algorithms"))
+#print(censor_text_multiple(email_two, proprietary_terms))
